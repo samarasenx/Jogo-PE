700cbb541507d477ded7445300dd8b3e137719d9
samarasenx <samaraestudos74@gmail.com>
2025-07-04 00:53:42 -0300

    Update personagem.h

--- livrojogo/personagem.h
+++ livrojogo/personagem.h
@@ -1,1 +1,19 @@
+#ifndef PERSONAGEM_H
+#define PERSONAGEM_H
 
+typedef struct {
+    char nome[TAM_NOME];
+    int sorte;
+    int carisma;
+    int estilo;
+    int inteligencia;
+    int amorHeroi;
+    int amorVilao;
+    int itens[MAX_ITENS];
+    int num_itens;
+    int capitulo_atual;
+    int caminho; // 0 = Gabriel, 1 = Lucas
+    int quests;
+} PERSONAGEM;
+
+#endif
